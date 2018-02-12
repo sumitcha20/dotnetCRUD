8ac40e329519e86dcd1e811ce5c56fd876db34e5
sumit <chaturvedi.sumit@tftus.com>
2018-02-12 15:26:37 +0530

    Created CRUD operation using dot net

---
+++ DatatableCRUD/Models/Extended/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatatableCRUD.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+
+    public class EmployeeMetadata
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please Provide Firstname")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Lastname")]
+        public string LastName { get; set; }
+    }
+}
